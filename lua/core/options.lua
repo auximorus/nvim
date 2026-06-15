b02a1a13fa339adf136a517b24a09fb0f0cdb7f7
auximorus <nilesh111305@gmail.com>
2026-06-15 22:44:57 +0530

    add options for enabling code folding

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,8 +2,14 @@
 local M = {}
 
 M.bools = { number = true, relativenumber = false, title = true }
-M.val = { shiftwidth = 4, tabstop = 4 }
-M.strs = { clipboard = "unnamedplus" }
+M.val = { shiftwidth = 4, tabstop = 4, foldlevel = 99, foldlevelstart = 1, foldnestmax = 4 }
+M.strs = {
+  clipboard = "unnamedplus",
+  foldmethod = "expr",
+  foldexpr = "v:lua.vim.treesitter.foldexpr()",
+  foldtext = "",
+  foldcolumn = "0",
+}
 M.lst = {}
 
 -- vim.g.skip_ts_context_commentstring_module = true
